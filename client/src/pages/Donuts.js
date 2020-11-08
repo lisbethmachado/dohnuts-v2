cc08981c6f7bc707ba106a441ae7b5012ee1b815
Lisbeth Machado <lisbeth@MacBook-Pro.local>
2020-11-08 15:53:33 -0800

    created imput element to post new donut to db

--- client/src/pages/Donuts.js
+++ client/src/pages/Donuts.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import DeleteBtn from "../components/DeleteBtn";
+import { Input, FormBtn } from "../components/Form"
 import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
@@ -8,6 +9,7 @@ import { List, ListItem } from "../components/List";
 function Donuts() {
   // Setting our component's initial state
   const [donuts, setDonuts] = useState([])
+  const [formObject, setFormObject] = useState({})
 
 
   useEffect(() => {
@@ -26,9 +28,43 @@ function Donuts() {
       .catch(err => console.log(err));
   }
 
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setFormObject({...formObject, [name]: value})
+  };
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    if (formObject.title) {
+      API.saveDonut({
+        title: formObject.title,
+      })
+        .then(res => loadDonuts())
+        .catch(err => console.log(err));
+    }
+  };
+
   return (
     <Container fluid>
     <Row>
+    <Col size="md-4 sm-12">
+          <Jumbotron>
+            <h1>Bake Donuts?</h1>
+          </Jumbotron>
+          <form>
+            <Input
+              onChange={handleInputChange}
+              name="title"
+              placeholder="Title (required)"
+            />
+            <FormBtn
+              disabled={!(formObject.title)}
+              onClick={handleFormSubmit}
+            >
+              Bake
+              </FormBtn>
+          </form>
+        </Col>
       <Col size="md-4 sm-12">
           <Jumbotron><h1>Dohnuts!</h1></Jumbotron>
           {donuts.length ? (
