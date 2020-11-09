fa8447a1a76869dce4f235979e40f3860671eadf
Lisbeth Machado <lisbeth@MacBook-Pro.local>
2020-11-08 17:16:32 -0800

    fixing api routes when server loads

--- server.js
+++ server.js
@@ -10,13 +10,11 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// Add "routes" below => app.use(routes), both API and view
+
 app.use(routes);
 
-// Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dohnuts");
 
-// Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
