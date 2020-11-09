3e86571e56475ad9cebea98ef1ffc686d3816320
Lisbeth Machado <lisbeth@MacBook-Pro.local>
2020-11-08 16:49:29 -0800

    added potential background images for app

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Donuts from './pages/Donuts';
 import NoMatch from "./pages/NoMatch";
+import donuts from "./images/donuts-yellow.jpg"
 import './App.css';
 
 function App() {
   return (
 <Router>
-      <div>
+      <div styles={{ backgroundImage:`url(${donuts})` }}>
         <Switch>
           <Route exact path={["/", "/books"]}>
             <Donuts />
