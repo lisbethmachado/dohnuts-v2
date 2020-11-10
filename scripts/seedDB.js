de73f9ed694d9aa74ba7941115cca6e3351dc1de
Lisbeth Machado <lisbeth@MacBook-Pro.local>
2020-11-10 12:09:54 -0800

    changed seed script env variable

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const db = require("../models");
 
 mongoose.connect(
-  process.env.MONGODB_URI ||
+  process.env.MONGO_Atlas ||
   "mongodb://localhost/dohnuts"
 );
 
