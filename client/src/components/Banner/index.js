44f1aa6ee19d8ad8b0044308357e4beda996114f
Lisbeth Machado <lisbeth@MacBook-Pro.local>
2020-11-21 20:07:25 -0800

    updated banner title styles

--- client/src/components/Banner/index.js
+++ client/src/components/Banner/index.js
@@ -3,13 +3,13 @@ import icon from '../../images/donut.png';
 
 function Banner() {
     return (
-<div style={{paddingTop: 15}} className="media">
+<div style={{paddingTop: 15, paddingBottom: 15, paddingLeft: 15, paddingRight: 15}} className="media">
   <div className="media-body">
     <h1 className="title">D'ohnuts</h1>
-    <p className="description">An interactive tribute to Homer Simpson and his love for donuts. Envision yourself as Homer Simpson and Create any donut you can imagine!
-  </p>
+    {/* <p className="description">An interactive tribute to Homer Simpson and his love for donuts. Envision yourself as Homer Simpson and Create any donut you can imagine!
+  </p> */}
   </div>
-  <img src={icon} style={{paddingTop: 15, paddingRight: 15}} className="ml-3" alt="donut" />
+  <img src={icon} style={{marginRight: 50}} className="ml-3" alt="donut" />
 </div>
 );
 }
