e3cecd901f79cd62bc0562c8e4d8fae6c689edf0
Lisbeth Machado <lisbeth@MacBook-Pro.local>
2020-11-21 19:43:31 -0800

    added background to title

--- client/src/components/Banner/index.js
+++ client/src/components/Banner/index.js
@@ -3,13 +3,13 @@ import icon from '../../images/donut.png';
 
 function Banner() {
     return (
-<div className="media">
+<div style={{paddingTop: 15}} className="media">
   <div className="media-body">
     <h1 className="title">D'ohnuts</h1>
-    <p className="description">Help Homer Simpson create unique, delicious donut recipes, then Bake, Eat and Repeat!
+    <p className="description">An interactive tribute to Homer Simpson and his love for donuts. Envision yourself as Homer Simpson and Create any donut you can imagine!
   </p>
   </div>
-  <img src={icon} className="ml-3" alt="donut" />
+  <img src={icon} style={{paddingTop: 15, paddingRight: 15}} className="ml-3" alt="donut" />
 </div>
 );
 }
