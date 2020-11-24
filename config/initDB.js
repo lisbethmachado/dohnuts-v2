c829f26e00b81f80eb7858d831310da35cecce0e
Lisbeth Machado <lisbeth@MacBook-Pro.local>
2020-11-23 16:03:46 -0800

    fixed donut create bug

--- config/initDB.js
+++ config/initDB.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 
 const mongoUri = process.env.MONGODB_URI || "mongodb://localhost:27017/donuts";
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://lisbethmachado:donuts@cluster0.alvtr.mongodb.net/donuts?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("donuts").collection("donuts");
-  // perform actions on the collection object
-  client.close();
-});
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://lisbethmachado:donuts@cluster0.alvtr.mongodb.net/donuts?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("donuts").collection("donuts");
+//   // perform actions on the collection object
+//   client.close();
+// });
 
 const dbOptions = {
   useNewUrlParser: true,
--- controllers/donutsController.js
+++ controllers/donutsController.js
@@ -5,7 +5,7 @@ module.exports = {
     db.Donut
       .find(req.query)
       .sort({ date: -1 })
-      .then(dbModel => res.json(dbModel) + console.log("Results: " + dbModel))
+      .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
